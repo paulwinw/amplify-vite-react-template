611d988b6381e95f5bd8a478cd8b8c81c1cc09c8
paulwinw <fsfunwang@gmail.com>
2025-02-17 08:45:40 -0800

    added authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import type { Schema } from '../amplify/data/resource';
+import { useAuthenticator } from '@aws-amplify/ui-react';
+import { useEffect, useState } from 'react';
+import { generateClient } from 'aws-amplify/data';
 
 const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const { signOut } = useAuthenticator();
 
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
+    const subscription = client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
+      error: (err) => console.error("Error in subscription:", err),
     });
+    return () => subscription.unsubscribe();
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+    const content = window.prompt("Todo content");
+    if (content) {
+      client.models.Todo.create({ content }).catch((error) => {
+        console.error("Failed to create todo:", error);
+        // Consider adding user feedback here
+      });
+    }
+  }
+
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id }).catch((error) => {
+      console.error("Failed to delete todo:", error);
+      // Consider adding user feedback here
+    });
   }
 
   return (
@@ -23,7 +40,14 @@ function App() {
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li 
+            onClick={() => deleteTodo(todo.id)}
+            key={todo.id}
+            role="button" // Improve accessibility
+            tabIndex={0}  // Make it focusable
+          >
+            {todo.content}
+          </li>
         ))}
       </ul>
       <div>
@@ -33,8 +57,10 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
 
 export default App;
+
